bb0b8762ea27834946f85119c063bc068dba4a65
Harri Kauhanen <harri.kauhanen@gmail.com>
2010-09-21 15:39:30 +0300

    looks better

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -44,7 +44,7 @@ class NotesController < ApplicationController
 
     respond_to do |format|
       if @note.save
-        format.html { redirect_to(@note, :notice => 'Note was successfully created.') }
+        format.html { redirect_to(notes_url, :notice => 'Note was successfully created.') }
         format.xml  { render :xml => @note, :status => :created, :location => @note }
       else
         format.html { render :action => "new" }
@@ -60,7 +60,7 @@ class NotesController < ApplicationController
 
     respond_to do |format|
       if @note.update_attributes(params[:note])
-        format.html { redirect_to(@note, :notice => 'Note was successfully updated.') }
+        format.html { redirect_to(notes_url, :notice => 'Note was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,9 @@ class Note
   include Mongoid::Document
   include Mongoid::Timestamps
   field :text, :type => String
+  
+  field :color, :type => Symbol
+  COLORS = [:yellow, :green, :purple]
+  validates_inclusion_of :color, :in => COLORS
+  
 end
